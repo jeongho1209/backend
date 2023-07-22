b2e2100b367365c664cdd8bcb3b4a662b7a3616f
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-07-22 21:08:56 +0900

    ⚡️ :: log 추가

--- third-assignment/src/main/kotlin/com/example/thirdassignment/check/CheckController.kt
+++ third-assignment/src/main/kotlin/com/example/thirdassignment/check/CheckController.kt
@@ -3,12 +3,18 @@ package com.example.thirdassignment.check
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.logging.Logger
 
 @RestController
 class CheckController {
 
+    private val logger = Logger.getLogger(this.javaClass.name)
+
     @GetMapping("/hello")
-    fun checkHello() = "Hello World!"
+    fun checkHello(): String {
+        logger.info("qwertyuiopasdfghjklzxcvbnm")
+        return "Hello World!"
+    }
 
     @GetMapping("/add")
     fun addTwoNumber(
