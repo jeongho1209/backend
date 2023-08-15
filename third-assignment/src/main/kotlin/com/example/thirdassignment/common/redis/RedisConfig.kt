24a4776f920d8a4a749aa32163ef65942ce3e66c
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-08-15 21:57:16 +0900

    .

--- third-assignment/src/main/kotlin/com/example/thirdassignment/common/redis/RedisConfig.kt
+++ third-assignment/src/main/kotlin/com/example/thirdassignment/common/redis/RedisConfig.kt
@@ -2,9 +2,9 @@ package com.example.thirdassignment.common.redis
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
-import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
 class RedisConfig(
@@ -12,15 +12,13 @@ class RedisConfig(
 ) {
 
     @Bean
-    fun redisConnectionFactory() = LettuceConnectionFactory(
-        redisProperties.host,
-        redisProperties.port,
-    )
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val redisConfig = RedisStandaloneConfiguration(redisProperties.host, redisProperties.port)
 
-    @Bean
-    fun redisTemplate() = RedisTemplate<String, String>().apply {
-        connectionFactory = redisConnectionFactory()
-        keySerializer = StringRedisSerializer()
-        valueSerializer = StringRedisSerializer()
+        if (redisProperties.password.isNotBlank()) {
+            redisConfig.setPassword(redisProperties.password)
+        }
+
+        return LettuceConnectionFactory(redisConfig)
     }
 }
