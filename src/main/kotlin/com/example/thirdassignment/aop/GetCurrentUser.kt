38713bdbcd298f5e6c2f1b56d568dc4b303e02cd
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-09-30 18:10:30 +0900

    ♻️ :: null 허용하지 않도록 수정

--- src/main/kotlin/com/example/thirdassignment/aop/GetCurrentUser.kt
+++ src/main/kotlin/com/example/thirdassignment/aop/GetCurrentUser.kt
@@ -2,6 +2,7 @@ package com.example.thirdassignment.aop
 
 import com.example.thirdassignment.user.domain.UserEntity
 import com.example.thirdassignment.user.domain.UserRepository
+import com.example.thirdassignment.user.exception.UserNotFoundException
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 
@@ -9,8 +10,9 @@ import org.springframework.stereotype.Component
 class GetCurrentUser(
     private val userRepository: UserRepository,
 ) {
-    fun getCurrentUser(): UserEntity? {
+    fun getCurrentUser(): UserEntity {
         val accountId = SecurityContextHolder.getContext().authentication?.name
         return userRepository.findByAccountId(accountId)
+            ?: throw UserNotFoundException
     }
 }
