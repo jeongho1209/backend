6ee5dda3a7ccdb953f99d28ab6287937ed8babf7
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-10-04 09:45:03 +0900

    ♻️ :: findAll 오버라이딩

--- src/main/kotlin/com/example/thirdassignment/todo/domain/TodoRepository.kt
+++ src/main/kotlin/com/example/thirdassignment/todo/domain/TodoRepository.kt
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository
 
 interface TodoRepository : CrudRepository<TodoEntity, Int> {
     fun findAllByUser(user: UserEntity): List<TodoEntity>
+    override fun findAll(): List<TodoEntity>
 }
